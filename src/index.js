58bd006c7b03aa7563ca4b5d4d6937cec126c7dc
DimaSavchuk <118117711+DimaSavchuk@users.noreply.github.com>
2023-05-03 14:00:56 +0300

    btn visibility

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { onSubmit } from "./js/requests/onSubmit";
 import refs from "./js/services/refs";
 
+refs.loadMoreEl.style.visibility = 'hidden';
 refs.formInputEl.addEventListener('submit', onSubmit)
